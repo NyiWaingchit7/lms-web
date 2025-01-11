6048cb62e83bac0121879bc582e07eacd16d6e16
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2025-01-11 15:57:42 +0630

    404-page

--- src/component/error/NotFount.tsx
+++ src/component/error/NotFount.tsx
@@ -7,9 +7,9 @@ export const NotFound = () => {
   return (
     <Layout>
       <Title title="404" />
-      <div className="h-[60vh] flex justify-center items-center">
+      <div className="h-[80vh] flex justify-center items-center">
         <div className="text-center flex flex-col justify-center items-center gap-5">
-          <h3 className="text-8xl not-found cherry-bomb-one-regular"> 404 </h3>
+          <h3 className="text-9xl not-found cherry-bomb-one-regular"> 404 </h3>
           <Link
             to={"/"}
             className="login-btn w-30 font-medium !text-green hover:!text-white text-lg"
