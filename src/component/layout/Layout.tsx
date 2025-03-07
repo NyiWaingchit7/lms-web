e2e636ec580ce8cc91b816e17fbbe4ad6ef4143b
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2025-03-07 14:35:44 +0630

    change-some

--- src/component/layout/Layout.tsx
+++ src/component/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { useAppDispatch } from "@/store/hooks";
 import { getAppSetting } from "@/store/slice/appSlice";
 import { ReactNode, useEffect } from "react";
-import { TagLine } from "./Tagline";
 import { NavBar } from "./Navbar";
 import { Footer } from "./Footer";
 import { getProfile } from "@/store/slice/authSlice";
